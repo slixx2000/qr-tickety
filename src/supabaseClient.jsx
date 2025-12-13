b304cf3f103968fc6f587512d374c776c535068a
Shaun_Shane <70473379+slixx2000@users.noreply.github.com>
2025-12-13 13:18:24 +0200

    Update Supabase redirect URL

--- src/supabaseClient.jsx
+++ src/supabaseClient.jsx
@@ -3,5 +3,5 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, { auth: { redirectTo: "https://unglaring-gianna-noneligibly.ngrok-free.dev"}});
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, { auth: { redirectTo: "https://qrtickety.netlify.app/"}});
 
